e1b727a1d1d8997e6cacecd9e302091dca6defe4
Vikas Jaiswal <vikasjaiswal.coder@gmail.com>
2025-04-29 13:01:47 +0530

    Revoked All the Exposed API Keys and Generated New Ones

--- app/src/main/java/dev/falcon/garagefinderpro/UserNavigationActivity.kt
+++ app/src/main/java/dev/falcon/garagefinderpro/UserNavigationActivity.kt
@@ -1,13 +1,16 @@
 package dev.falcon.garagefinderpro
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.FrameLayout
 import com.mapbox.navigation.base.options.NavigationOptions
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
+import com.mapbox.navigation.dropin.NavigationView
 import com.mapbox.navigation.ui.speedlimit.api.MapboxSpeedLimitApi
 import com.mapbox.navigation.ui.speedlimit.model.PostedAndCurrentSpeedFormatter
 import com.mapbox.navigation.ui.speedlimit.model.SpeedLimitFormatter
@@ -18,17 +21,17 @@ class UserNavigationActivity : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.usernavigation_activity, container, false)
+        val rootView = inflater.inflate(R.layout.usernavigation_activity, container, false)
 
+        // Setup Mapbox if not already done
         if (!MapboxNavigationApp.isSetup()) {
             MapboxNavigationApp.setup {
                 NavigationOptions.Builder(requireContext())
-                    .accessToken(getString(R.string.mapbox_access_token))
+                    .accessToken(BuildConfig.MAPBOX_DOWNLOADS_TOKEN)  // Ensure you use your valid access token here
                     .build()
             }
         }
 
-        return view
+        return rootView
     }
-
 }
